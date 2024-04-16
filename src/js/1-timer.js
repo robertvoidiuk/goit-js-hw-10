c4981a3cb63538bf95135aa32b6e6be45cbc90a6
robertvoidiuk <153754533+robertvoidiuk@users.noreply.github.com>
2024-04-16 17:59:59 +0300

    Task-1 completed

---
+++ src/js/1-timer.js
@@ -0,0 +1,91 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const input = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+const display = document.querySelector('.timer');
+
+const iziSettings = {
+  message: 'Please choose a date in the future',
+  messageColor: '#fff',
+  messageSize: '16px',
+  titleLineHeight: '24px',
+  backgroundColor: '#EF4040',
+  progressBarColor: '#B51B1B',
+  theme: 'dark',
+  position: 'topRight',
+};
+
+let userSelectedDate;
+
+startBtn.disabled = true;
+
+startBtn.addEventListener('click', onClick);
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < options.defaultDate) {
+      startBtn.disabled = true;
+      return iziToast.show(iziSettings);
+    }
+    input.disabled = false;
+    startBtn.disabled = false;
+    userSelectedDate = selectedDates[0];
+  },
+};
+
+flatpickr(input, options);
+
+function onClick() {
+  startBtn.disabled = true;
+  input.disabled = true;
+
+  const intervalId = setInterval(() => {
+    const currentDate = Date.now();
+    const diff = userSelectedDate - currentDate;
+    render(diff);
+
+    if (diff <= 1000) {
+      clearInterval(intervalId);
+      input.disabled = false;
+      iziToast.success({
+        message: 'Thank you for your visit:)',
+        position: 'topRight',
+        messageColor: '#fff',
+        backgroundColor: '#59a10d',
+        progressBarColor: '#326101',
+      });
+    }
+  }, 1000);
+}
+
+function render(diff) {
+  const { days, hours, minutes, seconds } = convertMs(diff);
+  display.querySelector('[data-days]').textContent = addLeadingZero(days);
+  display.querySelector('[data-hours]').textContent = addLeadingZero(hours);
+  display.querySelector('[data-minutes]').textContent = addLeadingZero(minutes);
+  display.querySelector('[data-seconds]').textContent = addLeadingZero(seconds);
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  return { days, hours, minutes, seconds };
+}
